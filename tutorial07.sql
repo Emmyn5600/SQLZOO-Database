0d718350727c404c9dc3bd5bfcef10c55cac11f2
emmy <emmy66418@gmail.com>
2021-04-06 17:10:32 +0200

    I complete the tutorial 6 and 7

--- tutorial07.sql
+++ tutorial07.sql
@@ -1,4 +1,4 @@
--- SQL tasks from sqlzoo.net
+-- SQL tasks from https://sqlzoo.net/wiki/SQL_Tutorial
 
 /*
 ***************************************************************
@@ -8,50 +8,55 @@
 ***************************************************************
 */
 
--- 1-Task solution:
+-- 1-Task solution: List the films where the yr is 1962 [Show id, title]
 SELECT id, title
  FROM movie
  WHERE yr=1962
 
 
--- 2-Task solution:
+-- 2-Task solution: Give year of 'Citizen Kane'.
 SELECT yr
 FROM movie
 WHERE title = 'Citizen Kane'
 
 
--- 3-Task solution:
+-- 3-Task solution: List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+
 SELECT id, title, yr
  FROM movie
  WHERE title LIKE '%Star Trek%'
  ORDER BY yr
 
 
--- 4-Task solution:
+-- 4-Task solution: What id number does the actor 'Glenn Close' have?
+
 SELECT id
  FROM actor
  WHERE name = 'Glenn Close'
 
 
--- 5-Task solution:
+-- 5-Task solution: What is the id of the film 'Casablanca'
+
 SELECT id
 FROM movie
 WHERE title =  'Casablanca' 
 
 
--- 6-Task solution:
+-- 6-Task solution: The cast list is the names of the actors who were in the movie. Use movieid=11768, (or whatever value you got from the previous question)
+
 SELECT name
 FROM casting JOIN actor ON casting.actorid = actor.id
 WHERE movieid=11768
 
 
--- 7-Task solution:
+-- 7-Task solution: Obtain the cast list for the film 'Alien'
 SELECT name
 FROM casting JOIN actor ON casting.actorid = actor.id
 WHERE movieid = (SELECT id FROM movie WHERE title = 'Alien')
 
 
--- 8-Task solution:
+-- 8-Task solution: List the films in which 'Harrison Ford' has appeared
+
 SELECT title
 FROM casting 
   JOIN movie ON casting.movieid = movie.id
@@ -59,7 +64,8 @@ FROM casting
 WHERE name = 'Harrison Ford'
 
 
--- 9-Task solution:
+-- 9-Task solution: List the films where 'Harrison Ford' has appeared - but not in the starring role. [Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role]
+
 SELECT title
 FROM casting 
   JOIN movie ON casting.movieid = movie.id
@@ -67,7 +73,8 @@ FROM casting
 WHERE name = 'Harrison Ford' AND ord != 1
 
 
--- 10-Task solution:
+-- 10-Task solution: List the films together with the leading star for all 1962 films.
+
 SELECT title, name
 FROM casting 
   JOIN movie ON casting.movieid = movie.id
@@ -75,7 +82,8 @@ FROM casting
 WHERE yr = 1962 AND ord = 1
 
 
--- 11-Task solution:
+-- 11-Task solution: Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies
+
 SELECT yr,COUNT(title) FROM
   movie JOIN casting ON movie.id=movieid
         JOIN actor   ON actorid=actor.id
@@ -84,7 +92,8 @@ GROUP BY yr
 HAVING COUNT(title) > 2
 
 
--- 12-Task solution:
+-- 12-Task solution: List the film title and the leading actor for all of the films 'Julie Andrews' played in.
+
 SELECT title, name
 FROM movie JOIN casting ON (movieid = movie.id
                             AND ord=1)
@@ -96,14 +105,16 @@ WHERE movie.id IN (
               WHERE name='Julie Andrews'))
 
 
--- 13-Task solution:
+-- 13-Task solution: Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles
+
 SELECT name
 FROM actor
   JOIN casting ON (id = actorid AND (SELECT COUNT(ord) FROM casting WHERE actorid = actor.id AND ord=1)>=15)
 GROUP BY name
 
 
--- 14-Task solution:
+-- 14-Task solution: List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+
 SELECT title, COUNT(actorid)
 FROM movie JOIN casting ON (movie.id = movieid)
 WHERE yr = 1978
@@ -111,7 +122,7 @@ GROUP BY title
 ORDER BY COUNT(actorid) DESC, title
 
 
--- 15-Task solution:
+-- 15-Task solution: List all the people who have worked with 'Art Garfunkel'.
 SELECT name
 FROM actor JOIN casting ON (actor.id = actorid)
 WHERE movieid IN
